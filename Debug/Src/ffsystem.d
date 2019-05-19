c05f9a9a46fdbc6db6282c8c0327b154f1ffe137
ycek21 <39658861+ycek21@users.noreply.github.com>
2019-05-19 23:06:31 +0200

    Commit

--- Debug/Src/ffsystem.d
+++
@@ -1,7 +0,0 @@
-Src/ffsystem.o: ../Src/ffsystem.c \
- E:/Studia/4\ SEMESTR/Podstawy\ techniki\ mikroprocesorowej/Projekt/2019_GGButton2/Inc/ff.h \
- E:/Studia/4\ SEMESTR/Podstawy\ techniki\ mikroprocesorowej/Projekt/2019_GGButton2/Inc/ffconf.h
-
-E:/Studia/4\ SEMESTR/Podstawy\ techniki\ mikroprocesorowej/Projekt/2019_GGButton2/Inc/ff.h:
-
-E:/Studia/4\ SEMESTR/Podstawy\ techniki\ mikroprocesorowej/Projekt/2019_GGButton2/Inc/ffconf.h:
--- Debug/Src/ffunicode.d
+++
@@ -1,7 +0,0 @@
-Src/ffunicode.o: ../Src/ffunicode.c \
- E:/Studia/4\ SEMESTR/Podstawy\ techniki\ mikroprocesorowej/Projekt/2019_GGButton2/Inc/ff.h \
- E:/Studia/4\ SEMESTR/Podstawy\ techniki\ mikroprocesorowej/Projekt/2019_GGButton2/Inc/ffconf.h
-
-E:/Studia/4\ SEMESTR/Podstawy\ techniki\ mikroprocesorowej/Projekt/2019_GGButton2/Inc/ff.h:
-
-E:/Studia/4\ SEMESTR/Podstawy\ techniki\ mikroprocesorowej/Projekt/2019_GGButton2/Inc/ffconf.h:
